4c091860ed9a4e05574d79d6c6905cd1f7d2876c
Maxim Legenkiy <mlegenkiy@karazin.ua>
2023-05-06 13:13:02 +0300

    add clear to foreground

--- my_app/background0.sh
+++ my_app/background0.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 apt update
 apt install tree
-echo "I am here!"
-cd /home/ubuntu
-alias dog='cat'
--- my_app/foreground3.sh
+++ my_app/foreground3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "backgound 3"
 cd /home/ubuntu
 alias cd='echo "command  cd is not allowed"'
+clear
