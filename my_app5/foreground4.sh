69cb3ce2197944cbf95d0e5a058bd5b54fc84199
Maxim Legenkiy <mlegenkiy@karazin.ua>
2024-07-14 18:07:07 +0300

    try fix foreground4.sh in lab5

--- my_app5/foreground4.sh
+++ my_app5/foreground4.sh
@@ -1,15 +1,12 @@
 cd
 poemfile=$HOME/poem
 j=1
-for i in {1..9}
+for i in {1..9};
 do 
-	dd if=/dev/urandom bs=$RANDOM count=1 of=file$i 2> /dev/null
-	if [ $(($RANDOM%2)) -eq 1 ]
+	dd if=/dev/urandom bs=$RANDOM count=1 of=file$i 2> /dev/null;
+	if [ $(($RANDOM%2)) -eq 1 ];
 	then
-		mkdir dir$j
-		cd dir$j
-		cp $poemfile .
-		j=$(($j+1))
+		mkdir dir$j && cd dir$j && cp $poemfile . && j=$(($j+1));
 	fi 
 done
 clear  
