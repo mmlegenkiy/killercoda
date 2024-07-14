bc72aa5545a1ddcf6f157e7bc1b979501fad678d
Maxim Legenkiy <mlegenkiy@karazin.ua>
2024-07-14 17:20:20 +0300

    fix problems?

--- my_app5/foreground4.sh
+++ my_app5/foreground4.sh
@@ -3,7 +3,7 @@ poemfile=$HOME/poem
 j=1
 for i in {1..9}
 do 
-	dd if=/dev/urandom bs=$RANDOM count=1 of=file$i;
+	dd if=/dev/urandom bs=$RANDOM count=1 of=file$i 2> /dev/null
 	if [ $(($RANDOM%2)) -eq 1 ]
 	then
 		mkdir dir$j
